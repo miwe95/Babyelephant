5bc06d52ea64b628cecdc40bd3369f1a9bd7dcee
miwe95 <miweiss95@gmail.com>
2020-12-02 18:17:52 +0100

    elephan moving

--- Baby Elephant Simulator/Assets/Scripts/ThirdPersonMovement.cs
+++ Baby Elephant Simulator/Assets/Scripts/ThirdPersonMovement.cs
@@ -4,27 +4,33 @@ using UnityEngine;
 
 public class ThirdPersonMovement : MonoBehaviour
 {
-    public CharacterController controller_;
-    public Transform cam_;
-    public float speed_ = 6f;
-    public float turn_smooth_time_ = 0.1f;
-    public float turn_smooth_velocity_;
-
-    // Update is called once per frame
-    void Update()
+    public float Speed = 5.0f;
+     Animator animator_;
+     void Start()
     {
-        float horizontal = Input.GetAxis("Horizontal");
-        float vertical = Input.GetAxis("Vertical");
-        Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
-        if(direction.magnitude >= 0.1f)
-        {
-            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam_.eulerAngles.y;
-            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turn_smooth_time_, turn_smooth_velocity_);
-            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+        //This gets the Animator, which should be attached to the GameObject you are intending to animate.
+        animator_ = gameObject.GetComponent<Animator>();
+        // The GameObject cannot jump
 
-            Vector3 move_dir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
-            controller_.Move(move_dir.normalized * speed_ * Time.deltaTime);
-        }
+    }
+	void Update ()
+    {
+        PlayerMovement();
+    }
+
+    void PlayerMovement()
+    {
+        float hor = Input.GetAxis("Horizontal");
+        float ver = Input.GetAxis("Vertical");
+
+        Vector3 playerMovement = new Vector3(hor, 0f, ver) * Speed * Time.deltaTime;
+        transform.Translate(playerMovement, Space.Self);
+        animator_.SetBool("is_running", true);
+         if(hor > 0 || ver > 0)
+            animator_.SetBool("is_running", true);
+
+        else
+            animator_.SetBool("is_running", false);
 
     }
 }
