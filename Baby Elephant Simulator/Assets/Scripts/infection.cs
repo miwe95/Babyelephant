1e8d961c85ac665f35eea70295c29397c1957fa7
Holder95 <lukas.holder@gmx.at>
2020-12-04 12:24:10 +0100

    first enemy
    added first enemy beans and made a simple patroling ai

---
+++ Baby Elephant Simulator/Assets/Scripts/infection.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class infection : MonoBehaviour
+{
+    public NavMeshAgent Agent;
+
+    public LayerMask whatIsGround;
+
+    public Vector3 walkPoint;
+    bool walkPointSet;
+    public float walkPointRange;
+
+    public bool hasCorona;
+
+    // Start is called before the first frame update
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        Patroling();
+    }
+
+    void Patroling()
+    {
+        if(!walkPointSet)
+        {
+            SearchWalkPoint();
+        }
+        else
+        {
+            Agent.SetDestination(walkPoint);
+        }
+
+        Vector3 distanceToWalkPoint = transform.position - walkPoint;
+
+        if(distanceToWalkPoint.magnitude < 1f)
+        {
+            walkPointSet = false;
+        }
+    }
+
+    void SearchWalkPoint()
+    {
+
+        float randomZ = Random.Range(-walkPointRange, walkPointRange);
+        float randomX = Random.Range(-walkPointRange, walkPointRange);
+
+        walkPoint = new Vector3(transform.position.x + randomX, transform.position.y, transform.position.z + randomZ);
+
+        if(Physics.Raycast(walkPoint, -transform.up, 2f, whatIsGround))
+        {
+            walkPointSet = true;
+        }
+    }
+}
