8f4de4de09732dd680cb153696c11f26ef20cf24
miwe95 <miweiss95@gmail.com>
2020-12-08 18:48:50 +0100

    movement update
    animations, jump fixed

--- Baby Elephant Simulator/Assets/Player/Scripts/MovementController.cs
+++ Baby Elephant Simulator/Assets/Player/Scripts/MovementController.cs
@@ -6,84 +6,110 @@ using UnityEngine;
 [RequireComponent(typeof(CharacterController))]
 public class MovementController : MonoBehaviour
 {
-    private float InputX, InputZ, Speed, Gravity = 0;
-    private Camera cam;
-    private CharacterController characterController;
+  private float InputX, InputZ, Speed, Gravity = 0;
+  private Camera cam;
+  private CharacterController characterController;
+  private Animator animator;
+  private Vector3 desiredMoveDirection;
 
-    private Vector3 desiredMoveDirection;
-    
-    [SerializeField] float rotationSpeed = 0.1f;
-    [SerializeField] float allowRotation = 0.1f;
-    [SerializeField] float movementSpeed = 10f;
-    [SerializeField] float gravityMultiplyer = 0.5f;
-    [SerializeField] float jumpHeight = 2f;
-    private Vector3 moveDirection;
+  [SerializeField] float rotationSpeed = 0.1f;
+  [SerializeField] float allowRotation = 0.1f;
+  [SerializeField] float movementSpeed = 10f;
+  [SerializeField] float gravityMultiplyer = 0.5f;
+  [SerializeField] float jumpHeight = 5f;
+  [SerializeField] float run_speed = 1f;
+  private Vector3 moveDirection;
+  private float gravityValue = -30.81f;
+  private Vector3 playerVelocity;
 
-    // Start is called before the first frame update
-    void Start()
+  // Start is called before the first frame update
+  void Start()
+  {
+    characterController = GetComponent<CharacterController>();
+    cam = Camera.main;
+    animator = GetComponent<Animator>();
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+    InputX = Input.GetAxis("Horizontal");
+    InputZ = Input.GetAxis("Vertical");
+
+
+    DesiredMoveDirection();
+    InputDecider();
+    MovementManager();
+  }
+
+  void InputDecider()
+  {
+    Speed = new Vector2(InputX, InputZ).sqrMagnitude;
+
+    if (Input.GetKey(KeyCode.LeftShift) && Speed > allowRotation)
     {
-        characterController = GetComponent<CharacterController>();
-        cam = Camera.main;
+      run_speed = 2;
+      animator.SetFloat("move", 1f);
+      RotationManager();
+    }
+    else if (Speed > allowRotation)
+    {
+      run_speed = 1;
+      animator.SetFloat("move", 0.3f);
+      RotationManager();
+    }
+    else
+    {
+      if (animator.GetFloat("move") > 0f)
+        animator.SetFloat("move", 0f);
+    }
+  }
+
+  void DesiredMoveDirection()
+  {
+    var forward = cam.transform.forward;
+    var right = cam.transform.right;
+    forward.y = 0;
+    right.y = 0;
+    forward.Normalize();
+    right.Normalize();
+
+    desiredMoveDirection = forward * InputZ + right * InputX;
+  }
+
+  void RotationManager()
+  {
+    transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(desiredMoveDirection), rotationSpeed);
+  }
+
+  void MovementManager()
+  {
+    if (characterController.isGrounded) Gravity = 0;
+
+    if (characterController.isGrounded && playerVelocity.y < 0)
+    {
+      playerVelocity.y = 0f;
     }
 
-    // Update is called once per frame
-    void Update()
+    Gravity -= 9.8f * Time.deltaTime;
+    Gravity *= gravityMultiplyer;
+
+    var verticalVelocity = 0.0f;
+
+    if (characterController.isGrounded && Input.GetKey(KeyCode.Space))
     {
-        InputX = Input.GetAxis("Horizontal");
-        InputZ = Input.GetAxis("Vertical");
-
-
-        DesiredMoveDirection();
-        InputDecider();
-        MovementManager();
+      //Gravity = jumpHeight * Time.deltaTime;
+      Debug.Log("Jumped");
+      playerVelocity.y += Mathf.Sqrt(jumpHeight * -3.0f * gravityValue);
+      animator.SetTrigger("jump");
     }
 
-    void InputDecider()
-    {
-        Speed = new Vector2(InputX, InputZ).sqrMagnitude;
+    Vector3 moveDirection = desiredMoveDirection * movementSpeed * run_speed * Time.deltaTime;
+    moveDirection = new Vector3(moveDirection.x, 0, moveDirection.z);
+    characterController.Move(moveDirection);
 
-        if (Speed > allowRotation)
-        {
-            RotationManager();
-        }
-    }
+    playerVelocity.y += gravityValue * Time.deltaTime;
+    characterController.Move(playerVelocity * Time.deltaTime);
 
-    void DesiredMoveDirection()
-    {
-        var forward = cam.transform.forward;
-        var right = cam.transform.right;
-        forward.y = 0;
-        right.y = 0;
-        forward.Normalize();
-        right.Normalize();
-
-        desiredMoveDirection = forward * InputZ + right * InputX;
-    }
-
-    void RotationManager()
-    {
-        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(desiredMoveDirection), rotationSpeed);
-    }
-
-    void MovementManager()
-    {
-        if (characterController.isGrounded) Gravity = 0;
-        Gravity -= 9.8f * Time.deltaTime;
-        Gravity *= gravityMultiplyer;
-
-        var verticalVelocity = 0.0f;
-
-        if (characterController.isGrounded && Input.GetButton("Jump")) {
-            // Gravity = jumpHeight * Time.deltaTime;
-            verticalVelocity = jumpHeight;
-        }
-
-        Vector3 jumpVector = new Vector3(0, verticalVelocity, 0);
-        characterController.Move(jumpVector * Time.deltaTime);
-
-        Vector3 moveDirection = desiredMoveDirection * movementSpeed * Time.deltaTime;
-        moveDirection = new Vector3(moveDirection.x, Gravity, moveDirection.z);
-        characterController.Move(moveDirection);
-
-    }
+  }
 }
