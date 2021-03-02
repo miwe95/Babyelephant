0e39ebec6d549d307f9e5ac1e575508c847ad759
miwe95 <miweiss95@gmail.com>
2021-03-02 15:28:30 +0100

    sound fix

--- Baby Elephant Simulator/Assets/Scripts/MusicPlayer.cs
+++ Baby Elephant Simulator/Assets/Scripts/MusicPlayer.cs
@@ -5,50 +5,48 @@ using UnityEngine.SceneManagement;
 
 public class MusicPlayer : MonoBehaviour
 {
-    private static MusicPlayer musicPlayerInstance;
+  private static MusicPlayer musicPlayerInstance;
 
-    private AudioSource audioSrc;
-    private float musicVolume = 1f;
+  public AudioSource audioSrc;
+  private float musicVolume = 1f;
 
-    // Start is called before the first frame update
-    void Start()
+  // Start is called before the first frame update
+  void Start()
+  {
+    // DontDestroyOnLoad (this);
+
+
+    AudioSource[] audios = FindObjectsOfType<AudioSource>();
+    if (audios.Length > 1)
     {
-        // DontDestroyOnLoad (this);
-        audioSrc = GetComponent<AudioSource>();
+      //audioSrc = GameObject.FindGameObjectWithTag("musicplayer_runforever").GetComponent<AudioSource>();
+      Destroy(audios[0]);
     }
-
-    // Update is called once per frame
-    void Update()
+    // if more then one music player is in the scene
+    //destroy ourselves
+    else
     {
-        audioSrc.volume = musicVolume;
+      DontDestroyOnLoad(audioSrc.gameObject);
     }
+    /*
+           Scene currentScene = SceneManager.GetActiveScene ();
+           string sceneName = currentScene.name;
+           if (sceneName == "fy_PoolParty")
+           {
+             DontDestroyOnLoad (transform.gameObject);
+           }
+    */
+  }
 
-    public void SetVolume(float vol)
-    {
-        musicVolume = vol;
-    }
 
-    private void Awake()
-    {
-      int numMusicPlayers = FindObjectsOfType<MusicPlayer>().Length;
-     if (numMusicPlayers != 1)
-     {
-         Destroy(this.gameObject);
-     }
-     // if more then one music player is in the scene
-     //destroy ourselves
-     else
-     {
-         DontDestroyOnLoad(gameObject);
-     }
-/*
-       Scene currentScene = SceneManager.GetActiveScene ();
-       string sceneName = currentScene.name;
-       if (sceneName == "fy_PoolParty")
-       {
-         DontDestroyOnLoad (transform.gameObject);
-       }
-*/
-    }
+  // Update is called once per frame
+  void Update()
+  {
+    audioSrc.volume = musicVolume;
+  }
 
+  public void SetVolume(float vol)
+  {
+    musicVolume = vol;
+  }
 }
