fdca4bedc3a37e26d704d1a5d579956d35aada3e
Jakob Rupitz <jakob.rupitz@student.tugraz.at>
2021-03-05 11:29:12 +0100

    Intro

---
+++ Baby Elephant Simulator/Assets/Scripts/Wait.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Wait : MonoBehaviour
+{
+
+    public float wait_time = 9.0f;
+
+    void Start()
+    {
+        StartCoroutine(WaitForIntro());
+    }
+
+    IEnumerator WaitForIntro()
+    {
+        yield return new WaitForSeconds(wait_time);
+        SceneManager.LoadScene("WelcomeScreen");
+    }
+}
